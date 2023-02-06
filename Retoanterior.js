51b5a1c629ca5665f2893d06c5f763e487fd44b8
Sara Sanchez <saritt@MacBook-Air-de-Sara.local>
2023-02-06 13:22:41 +0100

    creada funcion

---
+++ Retoanterior.js
@@ -0,0 +1,39 @@
+// - Declarar las funciones de la calculadora por separado (suma, resta, producto, división)
+(console.log("***** FUNCIONES SEPARADAS"))
+function sum (op1, op2) {
+//    let resultado = op1 + op2
+    return op1 + op2; //return resultado
+} 
+console.log(sum (2, 1));
+
+function subs (op1, op2){
+    //    let resultado = op1 - op2
+   return op1 - op2; //return resultado
+}
+console.log(subs (2, 1));
+
+function mult (op1, op2){
+    return op1 * op2;
+}
+console.log(mult (2, 1));
+
+function div (op1, op2){
+    return op1 / op2;
+}
+console.log(div (2, 1));
+
+// - Al final del documento index.js escribir la sentencia:
+// module.exports =
+(console.log("****module exports"))
+ module.exports =  { sum, subs, mult, div }
+//  let myLib = require("./index") en fichero run.js
+console.log("****RETO GIT")
+//5. Añade a este archivo de funciones, una nueva función que haga el cuadrado de un numero
+
+function numeroCuadrado (n){
+    let resultado = n ** 2
+    return resultado
+
+}
+
+console.log(numeroCuadrado(3));
